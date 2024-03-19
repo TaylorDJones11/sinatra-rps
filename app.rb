13c68013758689071f26a15240b87fbd3fad6544
Taylor D Jones <72883059+TaylorDJones11@users.noreply.github.com>
2024-03-19 15:03:54 +0000

    connecting the servers to the pages

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get('/') do
-  erb(:application_layout)
+  erb(:home)
+end
+
+get('/rock') do
+  erb(:rock)
+end
+
+get('/paper') do
+  erb(:paper)
+end
+
+get('/scissors') do
+  erb(:scissors)
 end
